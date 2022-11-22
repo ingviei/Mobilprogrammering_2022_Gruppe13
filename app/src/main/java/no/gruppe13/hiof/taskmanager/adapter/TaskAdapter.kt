60ab7bc896f0143e36612bb5b59dd1998da08de1
Ingvild Louise Eide <ingvild.l.eide@hiof.no>
2022-11-22 20:29:12 +0100

    Update TaskAdapter.kt

--- app/src/main/java/no/gruppe13/hiof/taskmanager/adapter/TaskAdapter.kt
+++ app/src/main/java/no/gruppe13/hiof/taskmanager/adapter/TaskAdapter.kt
@@ -3,25 +3,55 @@ package no.gruppe13.hiof.taskmanager.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import no.gruppe13.hiof.taskmanager.R
 import no.gruppe13.hiof.taskmanager.data.Task
+import no.gruppe13.hiof.taskmanager.databinding.TaskListItemBinding
 
-class TaskAdapter (private val taskItems: MutableList<Task>) : RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
+class TaskAdapter (private val taskItems: MutableList<Task>) : ListAdapter<Task, TaskAdapter.TaskViewHolder>(DiffCallback) {
 
-    class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class TaskViewHolder(private var binding: TaskListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(task: Task) {
+            binding.tvTaskTitle.text = task.title
+            binding.checkBoxItem.isChecked = task.completed // Sjekk i Task om skal være false default
+        }
+
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
         val viewHolder = TaskViewHolder(
-            LayoutInflater.from(parent.context), parent, false)}
+            TaskListItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+        return viewHolder
+        //viewHolder.itemView.
 
-    override fun onBindViewHolder(holder: TaskAdapter.TaskViewHolder, position: Int) {
+        //viewHolder.itemView.t
+    }
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+
         val currentTask = taskItems[position]
-        holder.itemView.apply {
-            holder.itemView.tv_taskTitle
-        }
+        holder.bind(currentTask)
+
     }
 
     override fun getItemCount(): Int {
     return taskItems.size    }
+
+    companion object {
+        private val DiffCallback = object : DiffUtil.ItemCallback<Task>() {
+            override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
 }
