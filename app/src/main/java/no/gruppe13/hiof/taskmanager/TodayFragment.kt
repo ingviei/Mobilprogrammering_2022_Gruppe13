0c28b1e9fd99cb6d54437818cdf9b30400c078ee
Ingvild Louise Eide <ingvild.l.eide@hiof.no>
2022-11-22 22:31:35 +0100

    Made togglestrikethrough

--- app/src/main/java/no/gruppe13/hiof/taskmanager/TodayFragment.kt
+++ app/src/main/java/no/gruppe13/hiof/taskmanager/TodayFragment.kt
@@ -1,12 +1,15 @@
 package no.gruppe13.hiof.taskmanager
 
+import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
 import android.os.Bundle
+import android.text.style.StrikethroughSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.CheckBox
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.coroutineScope
@@ -54,7 +57,7 @@ class TodayFragment : Fragment() {
         recyclerView = binding.rvToday
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
-        val taskAdapter = TaskAdapter { TaskDao.getAllTasks()}
+        val taskAdapter = TaskAdapter { TaskDao.getAllTasks() }
         recyclerView.adapter = taskAdapter
 
         GlobalScope.launch(Dispatchers.IO) {
@@ -65,21 +68,36 @@ class TodayFragment : Fragment() {
             }
         }
 
-        val checkbox = view.findViewById<CheckBox>(R.id.checkBoxItem)
-        checkbox.setOnCheckedChangeListener {compoundButton, b ->
-            Toast.makeText(this, Toast.LENGTH_SHORT).show()
-        })
 
-        private fun strikeThrough()
 
-        val addTaskButton = view.findViewById<Button>(R.id.btn_add_task)
+        // For å streke gjennom item
+       /* private fun toggleStrikeThrough(tvTaskTitle: TextView, completed: Boolean) {
+            if (completed) {
+                tvTaskTitle.paintFlags or STRIKE_THRU_TEXT_FLAG
+            } else {
+                tvTaskTitle.paintFlags = tvTaskTitle.paintFlags and STRIKE_THRU_TEXT_FLAG.inv()
+            }
+        }*/
 
-        addTaskButton.setOnClickListener{
-            val navController = it.findNavController()
-            navController.navigate(R.id.action_todayFragment_to_navigation_create_task)
-        }
+        //val checkbox = view.findViewById<CheckBox>(R.id.checkBoxItem)
+
+        //Wrap en funksjon rundt denne
+       /* toggleStrikeThrough(tvTaskTitle, currentTask.completed)
+        checkbox.setOnCheckedChangeListener { _, _ ->
+            toggleStrikeThrough(tvTaskTitle, completed)
+            currentTask.completed = !currentTask.completed
+        }*/
 
     }
 
 
-}
+       /* val addTaskButton = view.findViewById<Button>(R.id.btn_add_task)
+
+        addTaskButton.setOnClickListener{
+            val navController = it.findNavController()
+            navController.navigate(R.id.action_todayFragment_to_navigation_create_task)
+        }*/
+
+    }
+
+
